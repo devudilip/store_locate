b6328c68003b7c7231a598946a60ca656575c6f4
sasifunz <sasifunz@gmail.com>
2015-09-21 11:16:07 +0000

    Request to API

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :products
   resources :stores
   resources :store_products
+  resources :locate_stores do
+    collection do
+      post 'search'
+    end  
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
