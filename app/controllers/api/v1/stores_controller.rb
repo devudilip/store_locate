db6a915c722574749dc979d7aaabf4a709dddc14
devudilip <devu.dilip@gmail.com>
2015-09-21 16:14:04 +0530

    store product search

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -4,9 +4,13 @@ module Api
       respond_to :json
 
       def index
-        respond_with Product.search_availability(1,1)
+        if params[:store_id] and params[:product_id]
+          respond_with Product.search_availability(params[:store_id],params[:product_id])
+        else
+          respond_with {'false'}
+        end
       end
-      
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -62,8 +62,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
-    resources :stores
-  end
-end
+      resources :stores do
+       get 'product_avail', on: :collection
+     end
+   end
+ end
 
 end
