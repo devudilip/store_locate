46760135443226ffb3efb8e79697fc975be2831c
devudilip <devu.dilip@gmail.com>
2015-09-27 01:06:27 +0530

    other stores suggestion

--- app/models/store.rb
+++ app/models/store.rb
@@ -11,8 +11,9 @@ class Store < ActiveRecord::Base
     stores = Store.all
     stores.each do |store|
       response = Product.search_availability(store.id,product_ids)
-      found_stores << {store: store.id,count: response[:count], distance: store.store_distance, lat: store.lat, lng: store.long} if response[:count] > 0
-    end
+      found_stores << {store: store.id,count: response[:count], distance: store.store_distance, lat: store.lat, lng: store.long, store_name: store.name,
+                       address: store.address, store_distance: store.store_distance, store_type: store.store_type  } if response[:count] > 0
+     end
     # store_by_count = found_stores.sort_by { |store| store[:count] }.last
     # store_by_distance = found_stores.select{|store| store[:count] == store_by_count[:count]}.sort_by{|dis| dis[:distance]}.first[:store]
     return found_stores
