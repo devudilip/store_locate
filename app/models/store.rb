19af12dcf192e4083f95c2e57781628aa445eb50
devudilip <devu.dilip@gmail.com>
2015-09-26 23:52:30 +0530

    sort store based on count and distance

--- app/models/store.rb
+++ app/models/store.rb
@@ -11,11 +11,11 @@ class Store < ActiveRecord::Base
     stores = Store.all
     stores.each do |store|
       response = Product.search_availability(store.id,product_ids)
-      found_stores << {store: store.id,count: response[:count], distance: store.store_distance}
+      found_stores << {store: store.id,count: response[:count], distance: store.store_distance, lat: store.lat, lng: store.long} if response[:count] > 0
     end
-    store_by_count = found_stores.sort_by { |store| store[:count] }.last
-    store_by_distance = found_stores.select{|store| store[:count] == store_by_count[:count]}.sort_by{|dis| dis[:distance]}.first[:store]
-    return Store.find(store_by_distance)
+    # store_by_count = found_stores.sort_by { |store| store[:count] }.last
+    # store_by_distance = found_stores.select{|store| store[:count] == store_by_count[:count]}.sort_by{|dis| dis[:distance]}.first[:store]
+    return found_stores
   end
 
 end
