383cb5a3051e5f5195ee71e0035b5e6fef451cad
devudilip <devu.dilip@gmail.com>
2015-09-27 23:57:08 +0530

    small fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Seeds for the STore table
-Store.create(name: 'ST KATHARINES WAY EXPRESS',address:'ST KATHARINES WAY EXPRESS52ce9bad42632be6e1e8c992',lat:'51.506826',long:'-0.072624',location_id:'52ce9bad42632be6e1e8c992',store_distance:'0.3083144404782735',store_type:'Express')
-Store.create(name: 'MANSELL STREET EXPRESS',address:'MANSELL STREET EXPRESS52ce9bad42632be6e1e8d03b',lat:'51.51201268',long:'-0.073404789',location_id:'52ce9bad42632be6e1e8d03b',store_distance:'0.43127890942798386',store_type:'Express')
-Store.create(name: 'TOOLEY STREET METRO',address:'TOOLEY STREET METRO52ce9bad42632be6e1e8d1e3',lat:'51.50416048',long:'-0.081531428',location_id:'52ce9bad42632be6e1e8d1e3',store_distance:'0.6136432287768385',store_type:'Metro')
-Store.create(name: 'MONUMENT METRO',address:'MONUMENT METRO52ce9bad42632be6e1e8cd98',lat:'51.51058405',long:'-0.085008521',location_id:'52ce9bad42632be6e1e8cd98',store_distance:'0.6561243089876169',store_type:'Metro')
-Store.create(name: 'LONDON ST. BOTOLPHS EXPRESS',address:'LONDON ST. BOTOLPHS EXPRESS52ce9bad42632be6e1e8d176',lat:'51.51442573',long:'-0.076877281',location_id:'52ce9bad42632be6e1e8d176',store_distance:'0.6583359778213261',store_type:'Express')
+Store.create(name: 'ST KATHARINES WAY EXPRESS',address:'ST KATHARINES WAY EXPRESS',lat:'51.506826',long:'-0.072624',location_id:'52ce9bad42632be6e1e8c992',store_distance:'0.3083144404782735',store_type:'Express')
+Store.create(name: 'MANSELL STREET EXPRESS',address:'MANSELL STREET EXPRESS',lat:'51.51201268',long:'-0.073404789',location_id:'52ce9bad42632be6e1e8d03b',store_distance:'0.43127890942798386',store_type:'Express')
+Store.create(name: 'TOOLEY STREET METRO',address:'TOOLEY STREET METRO',lat:'51.50416048',long:'-0.081531428',location_id:'52ce9bad42632be6e1e8d1e3',store_distance:'0.6136432287768385',store_type:'Metro')
+Store.create(name: 'MONUMENT METRO',address:'MONUMENT METRO',lat:'51.51058405',long:'-0.085008521',location_id:'52ce9bad42632be6e1e8cd98',store_distance:'0.6561243089876169',store_type:'Metro')
+Store.create(name: 'LONDON ST. BOTOLPHS EXPRESS',address:'LONDON ST. BOTOLPHS EXPRESS',lat:'51.51442573',long:'-0.076877281',location_id:'52ce9bad42632be6e1e8d176',store_distance:'0.6583359778213261',store_type:'Express')
 
 # Seeds for the Products
 
